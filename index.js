a788cc7a088e57cc1a8da17c8193a1822cbb3e37
Deeksha Deepak <77612609+Deeksha1502@users.noreply.github.com>
2021-08-19 15:08:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const colors=["green","red", "rgba(133,122,200)", "#f15025"];
-const btn=document.getElementById("btn");
-const color=document.querySelector(".color");
-btn.addEventListrner("click", function(){
+const colors=["blue","red", "rgba(133,122,200)", "#f15025"];
+const btn1=document.getElementById("btn");
+const color1=document.querySelector(".color");
+btn1.addEventListerner("click", function(){
     const randomNumber=getRandomNumber();
     document.body.style.backgroundColor=colors[randomNumber];
-color.textContent=colors[randomNumber];
+color1.textContent=colors[randomNumber];
 });
 
 function getRandomNumber()
 {
-    return Math.random();
+    return Math.floor(Math.random()*4);
 }
